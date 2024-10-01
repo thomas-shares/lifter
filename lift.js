54f449b649cf6f3b68a3ce4bea4af3fe342ede8d
Van der Veen <thomas.vanderveen@nslogin.nl>
2024-10-01 18:48:14 +0200

    passed a level

--- lift.js
+++ lift.js
@@ -9,100 +9,50 @@
         // Whenever the elevator is idle (has no more queued destinations) ...
         elevator.on("idle", () => {
             // let's go to all the floors (or did we forget one?)
-            //evator.goToFloor(0);
-            console.log("Idle..")
-
-            if(elevator.getPressedFloors().length > 0) {
-                // Maybe go to some chosen floor first?
-                console.log("getPressedFloor: ", elevator.getPressedFloors())
-
-                // if( elevator.getPressedFloors().includes(floorNum)){
-                //   console.log("deze vloer zit er in!!!")
-                //   elevator.stop();
-                // }
-            }
-            
-            if(up.length > 0) {
-                console.log("up: ", up)
-                
-            }
-            
-            if( down.length > 0) {
-                console.log("down: ", down)
-            }
+            console.log("Idle...")
+            let highestUp = up.findLastIndex((x) => x === true)
+            let highestDown = down.findLastIndex((x) => x === true)
+            let highestDestivation = destination.findLastIndex((x) => x === true)
+            let max = Math.max(highestDestivation, highestDown, highestUp)
+            let min = Math.min(highestDestivation, highestDown, highestUp)
+            console.log("max: ", max, " min: ", min)
+            elevator.goToFloor(max)
         });
         
         elevator.on("floor_button_pressed", (floorNum) => {
             let currentFloor = elevator.currentFloor()
-            console.log("Floor button pressed: ", floorNum, " current floor: ", elevator.currentFloor())
-            //console.log(floors.length)
-            elevator.goToFloor(floorNum)
-            //console.log("Going to floor: ", floorNum)
+            console.log("Floor button pressed: ", floorNum, " current floor: ", currentFloor)
             destination[floorNum] = true
         });
         
         elevator.on("passing_floor", (floorNum, direction) => {
             console.log("passing_floor", floorNum, direction)
-            
-            if(elevator.destinationQueue.includes(floorNum)){
-                console.log("UNshift: " , floorNum)
-                console.log("destinationQueue: ", elevator.destinationQueue)
-            }
-            
 
-            if(elevator.getPressedFloors().length > 0) {
-                // Maybe go to some chosen floor first?
-                console.log(" getpressed: ", elevator.getPressedFloors())
-
-                if( elevator.getPressedFloors().includes(floorNum)){
-                    console.log("deze vloer zit er in!!!")
-                    //elevator.stop();
-                }
-            }
-            
-            if(up.includes(floorNum) || down.includes(floorNum)) {
-                elevator.destinationQueue.unshift(floorNum)
-            }
-            
-            if(up.includes(floorNum) && direction === "up" ) {
-                console.log("people waiting on ", floorNum, " going UP")
-                //elevator.stop();
-            }
-            
-            if(down.includes(floorNum) && direction === "down") {
-                console.log("peope waiting on ", floorNum, " going DOWN")
-                //elevator.stop();
-            }
-         });
+        });
         
         elevator.on("stopped_at_floor", (floorNum) => {
             // Maybe decide where to go next?
             console.log("stopped at floor", floorNum)
+            up[floorNum] = false
+            down[floorNum] = false
+            destination[floorNum] = false
         });
 
         floors.map((floor) => {
             let floorNum = floor.floorNum()
-            //console.log(floor)
+
             floor.on("up_button_pressed", () => {
                 console.log("up_button_pressed", floorNum)
-                //upSet.add(floorNum)
-                up = add(up, floorNum)
-                elevator.goToFloor(floorNum)
-
-                //console.log("upSet: ", upSet)
-               // debugger
+                up[floorNum] = true
             });
 
             floor.on("down_button_pressed", () => {
                 console.log("down_button_pressed", floorNum)
-                //downSet.add(floorNum)
-                elevator.goToFloor(floorNum)
-
-                down = add(down, floorNum)
+                down[floorNum] = true
              });
         });
 
-  },
+   },
     update: (dt, elevators, floors) => {
         // We normally don't need to do anything here
     }
